cef9674158bee7f03ae262fce826a83dcc4de433
Raman Siliuk <rsiluk@swiftinvention.com>
2024-09-24 13:17:16 +0400

    initializing the repository

---
+++ astro.config.mjs
@@ -0,0 +1,78 @@
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
+import starlight from "@astrojs/starlight";
+
+export const site = "https://browser.mee.foundation/";
+export const meeFoundation = "https://mee.foundation/";
+export const github = "https://github.com/MeeFoundation/mee-browser-website";
+export const discord =
+  "https://discord.com/channels/1275848491964436491/1275848492413223025";
+export const sharedConfig = {
+  title: "Mee Browser",
+  logo: {
+    light: "/src/assets/logo-light.svg",
+    dark: "/src/assets/logo-dark.svg",
+    replacesTitle: true,
+  },
+};
+// https://astro.build/config
+export default defineConfig({
+  output: "static",
+  adapter: node({
+    mode: "standalone",
+  }),
+  site: site,
+  redirects: {
+    "/docs": "/docs/users/mee-browser",
+  },
+  integrations: [
+    starlight({
+      ...sharedConfig,
+      disable404Route: true,
+      logo: {
+        light: "/src/assets/logo-light.svg",
+        dark: "/src/assets/logo-dark.svg",
+        replacesTitle: true,
+      },
+      editLink: {
+        baseUrl:
+          "https://github.com/meefoundation/mee-browser-website/edit/main/",
+      },
+      social: {
+        github: github,
+      },
+      customCss: process.env.NO_GRADIENTS ? [] : ["/src/assets/landing.css"],
+      locales: {
+        root: { label: "English", lang: "en" },
+      },
+      sidebar: [
+        {
+          label: "For Users",
+          items: [
+            {
+              label: "Common",
+              autogenerate: { directory: "docs/users/mee-browser" },
+            },
+          ],
+        },
+        {
+          label: "For Developers",
+          items: [
+            {
+              label: "Common",
+              autogenerate: {
+                directory: "docs/developers/mee-browser",
+              },
+            },
+          ],
+        },
+      ],
+      components: {
+        Sidebar: "./src/components/MultiSidebar.astro",
+        EditLink: "./src/components/EditLink.astro",
+      },
+    }),
+    tailwind(),
+  ],
+});
