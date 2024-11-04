341829c8dd4645c74473f33ac59e9fe3b3a83cfa
Raman Siliuk <rsiluk@swiftinvention.com>
2024-11-04 11:22:54 +0400

    changed discord link

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import starlight from "@astrojs/starlight";
 export const site = "https://browser.mee.foundation/";
 export const meeFoundation = "https://mee.foundation/";
 export const github = "https://github.com/MeeFoundation/mee-browser-website";
-export const discord =
-  "https://discord.com/channels/1275848491964436491/1275848492413223025";
+export const discord = "https://discord.gg/V4vC5zsZ";
 export const sharedConfig = {
   title: "Mee Browser",
   logo: {
